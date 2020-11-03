785753986d78db7cb5f37fd9bd08cd7709d49537
d1mak3 <d1mak3@yandex.ru>
2020-11-04 01:09:08 +0300

    v0.0.3 доделал макет клиента. Сделал отправку сообщений на сервер при нажатии кнопки

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,11 +12,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace ClientForMessenger
 {  
   public partial class MainWindow : Window
-  {  
+  {
+    MessagesOperationHandler operation = new MessagesOperationHandler();
+
     public MainWindow()
     {
       InitializeComponent();  
@@ -30,9 +33,44 @@ namespace ClientForMessenger
       this.Visibility = Visibility.Hidden;
       login.Show();
 
-      ContactsPanel.Children.Add(new TextBlock { Text = "Даня" });
-      MessagePanel.Children.Add(new TextBlock { Text = "Привет" });
-         
-    }    
+     
+      ContactsPanel.Children.Add(new TextBlock { Text = "Дима" }); // Пример юзера
+      MessagePanel.Children.Add(new TextBlock { Text = "Дима\nПривет" }); // Пример сообщения
+      // this.GetMessages()
+    }
+
+    private void Main_SizeChanged(object sender, SizeChangedEventArgs e) // Меняем ввод и кнопку в зависимости от размеров окошка
+    {
+      TypeTextBox.Width = this.Width / 1.6;
+      SendButton.Width = this.Width / 10;
+    }
+
+    public async void GetMessages()
+    {
+      Message get = new Message();
+      int currentID = 0;
+      await Task.Run(() =>
+      {
+        while (true)
+        {
+          operation.GetMessage(out get, currentID++);
+          if (get.message != "Message doesnt exist")
+          {
+            MessagePanel.Children.Add(new TextBlock { Text = $"{get.userName}\n{get.message}" });
+            ++currentID;
+          }
+        }        
+      });
+    }
+
+    private void SendButton_Click(object sender, RoutedEventArgs e) // Отправка сообщений
+    {
+      if (TypeTextBox.Text != String.Empty)
+      {
+        Message newMessage = new Message("Server", TypeTextBox.Text);
+        operation.SendMessage(newMessage);
+        TypeTextBox.Text = String.Empty;
+      }
+    }
   }
 }
